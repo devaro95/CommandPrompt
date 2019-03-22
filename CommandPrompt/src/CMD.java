3b709f97b911c16fc689b05bb702c88ef6d65403
jos.castro <jos.castro@pc1608>
2019-03-22 19:03:38 +0100

    asdj

--- CommandPrompt/src/CMD.java
+++ CommandPrompt/src/CMD.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class CMD {
 
-	public static void main (String args[]) throws IOException{
-		File miDir = new File("/datos/usuarios/alumnos/alvaro.monterocarmena/Escritorio/");
+	public static void main (String args[]) throws Exception{
+		String current = new java.io.File( "." ).getCanonicalPath();
+		File miDir = new File(current);
 		String  opcion="", comando[], nombre, contenido;
 		int numLineas;
 		Scanner teclado = new Scanner(System.in);
@@ -13,42 +14,60 @@ public class CMD {
 
 		String directorioActual;
 		while(!opcion.equals("exit")) {
+			java.net.InetAddress localMachine = java.net.InetAddress.getLocalHost();
+			String usuario = System.getProperty("user.name");
+			System.out.print(usuario + "@" + localMachine.getHostName() + ":" + miDir + " ");
 			opcion = teclado.nextLine();
 			comando = opcion.split(" ");
 			if(comando.length==1 && comando[0].equals("help")) {
-				opcion = "1";
-			}else if(comando.length==3 && comando[0].equals("mkfile")){
-				opcion = "2";
-			}else if(comando.length==2 && comando[0].equals("mkdir")) {
-				opcion = "3";
-			}else if(comando.length==2 && comando[0].equals("cat")) {
-				opcion = "4";
-			}else if(comando.length==3 && comando[0].equals("top")) {
-				opcion = "5";
-			}else if(comando.length>=3 && comando[0].equals("write")) {
-				opcion = "6";
-			}else if(comando.length==2 && comando[0].equals("cd") && !comando[1].equals("..") && comando[1].startsWith("/")) {
-				opcion = "8";
-			}else if(comando.length==2 && comando[0].equals("cd") && !comando[1].equals("..")) {
-				opcion = "7";
-			}
-			switch (opcion) {
-			case "1":
 				System.out.println("cd:\n  -cd : Mostrar directorio actual. \n  "
 						+ "-cd : Acceder al directorio padre.\n  "
 						+ "-cd <nombre directorio>: Acceder a un directorio \n  "
 						+ "-cd <C:\\Nombre_Directorio>: Accedee a una ruta absoluta del sistema.\n"
-						+ "-mkdir <nombre_directorio>: Creae directorio en la ruta actual.\n"
-						+ "-cat <nombre_fichero> : Mostrar contenido de un fichero a través del terminal.\n"
-						+ "-top <numero_lineas> <nombre_fichero>: Mostrar las primeras lineas de un fichero.\n"
-						+ "-mkfile <nombre_fichero> <texto>: Crear un nuevo fichero.\n"
-						+ "-write <nombre_fichero> <texto>: Añadir texto a un fichero existente.");
-				break;
-			case "cd":
-				directorioActual = cd.directorioActual(miDir);
-				System.out.println(directorioActual);
-				break;
-			case "cd ..":
+						+ "mkdir <nombre_directorio>: Creae directorio en la ruta actual.\n"
+						+ "cat <nombre_fichero> : Mostrar contenido de un fichero a través del terminal.\n"
+						+ "top <numero_lineas> <nombre_fichero>: Mostrar las primeras lineas de un fichero.\n"
+						+ "mkfile <nombre_fichero> <texto>: Crear un nuevo fichero.\n"
+						+ "write <nombre_fichero> <texto>: Añadir texto a un fichero existente.\n"
+						+ "Dir: Lista los archivos contenidos en el path actual.\n"
+						+ "Delete <nombre_archivo_o_directorio>: Borra un archivo o una carpeta, en caso de ser una\n" + 
+						"carpeta borra todos los archivos que la contienen.");
+			}else if(comando.length==3 && comando[0].equals("mkfile")){
+				nombre = comando[1];
+				contenido = comando[2];
+				cd.mkfile(nombre, contenido, miDir);
+			}else if(comando.length==2 && comando[0].equals("mkdir")) {
+				nombre = comando[1];
+				cd.mkdir(nombre, miDir);;
+			}else if(comando.length==2 && comando[0].equals("cat")) {
+				nombre = comando[1];
+				cd.cat(nombre, miDir);
+			}else if(comando.length==3 && comando[0].equals("top")) {
+				nombre = comando[1];
+				numLineas = Integer.parseInt(comando[2]);
+				cd.top(nombre, miDir, numLineas);
+			}else if(comando.length>=3 && comando[0].equals("write")) {
+				nombre = comando[1];
+				contenido = "\n";
+				for(int i=2; i<comando.length; i++) {
+					contenido += comando[i] + " ";
+				}
+				cd.write(nombre, miDir, contenido);
+			}else if(comando.length==2 && comando[0].equals("cd") && !comando[1].equals("..") && comando[1].startsWith("/")) {
+				nombre = comando[1];
+				try {
+					miDir = cd.moverAbsoluto(nombre);
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}else if(comando.length==2 && comando[0].equals("cd") && !comando[1].equals("..")) {
+				nombre = comando[1];
+				try {
+					miDir = cd.moverDirectorio(nombre, miDir);
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}else if(comando.length==2 && comando[0].equals("cd") && comando[1].equals("..")) {
 				String[] ruta = cd.directorioActual(miDir).split("/");
 				String nuevaRuta = "";
 				if(ruta.length>=3) {
@@ -60,46 +79,37 @@ public class CMD {
 					nuevaRuta = "/";
 				}
 				miDir = new File(nuevaRuta);
-				System.out.println(miDir.getPath());
-				break;
-			case "2":
+			}else if(comando.length==1 && comando[0].equals("cd")) {
+				directorioActual = cd.directorioActual(miDir);
+				System.out.println(directorioActual);
+			}else if(comando.length==1 && comando[0].equals("Dir")) {
+				directorioActual = cd.directorioActual(miDir);
+				File dir = new File(directorioActual);
+				String[] ficheros = dir.list();
+			    if (ficheros == null)
+			        System.out.println("No hay ficheros en el directorio especificado");
+			      else { 
+			        for (int x=0;x<ficheros.length;x++)
+			          System.out.println(ficheros[x]);
+			    }
+			}else if(comando.length==2 && comando[0].equals("Dir")) {
 				nombre = comando[1];
-				contenido = comando[2];
-				cd.mkfile(nombre, contenido, miDir);
-				break;
-			case "3":
+				directorioActual = cd.directorioActual(miDir);
+				File dir = new File(directorioActual + "/" + nombre);
+				String[] ficheros = dir.list();
+			    if (ficheros == null)
+			        System.out.println("No hay ficheros en el directorio especificado");
+			      else { 
+			        for (int x=0;x<ficheros.length;x++)
+			          System.out.println(ficheros[x]);
+			    }
+			}else if(comando.length==2 && comando[0].equals("Delete")) {
 				nombre = comando[1];
-				cd.mkdir(nombre, miDir);
-			case "4":
-				nombre = comando[1];
-				cd.cat(nombre, miDir);
-			case "5":
-				nombre = comando[1];
-				numLineas = Integer.parseInt(comando[2]);
-				cd.top(nombre, miDir, numLineas);
-			case "6":
-				nombre = comando[1];
-				contenido = "\n";
-				for(int i=2; i<comando.length; i++) {
-					contenido += comando[i] + " ";
-				}
-				cd.write(nombre, miDir, contenido);
-			case "7":
-				nombre = comando[1];
-				try {
-					miDir = cd.moverDirectorio(nombre, miDir);
-				} catch (Exception e) {
-					System.out.println(e);
-				}
-				break;
-			case "8":
-				nombre = comando[1];
-				try {
-					miDir = cd.moverAbsoluto(nombre);
-				} catch (Exception e) {
-					System.out.println(e);
-				}
+				cd.delete(nombre, miDir);
+			}else {
+				System.out.println("El comando '" + comando[0] + "' no existe");
 			}
+			
 		}
 	}
 }
--- CommandPrompt/src/CdCommand.java
+++ CommandPrompt/src/CdCommand.java
@@ -24,7 +24,7 @@ public class CdCommand {
 	public void mkdir(String nombre, File directorio) throws IOException {
 		File miDir = new File(directorioActual(directorio)+"/"+nombre);
 		miDir.mkdir();
-		System.out.println("asd");
+		System.out.println("Se ha creado el directorio '" + nombre + "'");
 	}
 	public void mkfile(String nombre, String contenido, File directorio) throws IOException {
 		File miDir = new File(directorioActual(directorio)+"/"+nombre);
@@ -34,25 +34,34 @@ public class CdCommand {
 		bw.write(contenido);
 		bw.close();
 	}
-	public void cat(String nombre, File directorio) throws IOException {
+	public void cat(String nombre, File directorio) throws Exception {
 		File miDir = new File(directorioActual(directorio)+"/"+nombre);
-		FileReader fr = new FileReader(miDir);
-		BufferedReader br = new BufferedReader(fr);
-		String linea;
-		while((linea=br.readLine())!=null) {
-			System.out.println(linea);
+		if(miDir.exists() && miDir.isFile()){
+			FileReader fr = new FileReader(miDir);
+			BufferedReader br = new BufferedReader(fr);
+			String linea;
+			while((linea=br.readLine())!=null) {
+				System.out.println(linea);
+			}
+		}else {
+			System.out.println("No es un fichero o no existe");
 		}
 	}
-	public void top(String nombre, File directorio, int numLineas) throws IOException {
+	public void top(String nombre, File directorio, int numLineas) throws Exception {
 		File miDir = new File(directorioActual(directorio)+"/"+nombre);
-		FileReader fr = new FileReader(miDir);
-		BufferedReader br = new BufferedReader(fr);
-		int contador = 0;
-		String linea;
-		while((linea=br.readLine())!=null && contador<numLineas) {
-			System.out.println(linea);
-			contador++;
+		if(miDir.exists() && miDir.isFile()){
+			FileReader fr = new FileReader(miDir);
+			BufferedReader br = new BufferedReader(fr);
+			int contador = 0;
+			String linea;
+			while((linea=br.readLine())!=null && contador<numLineas) {
+				System.out.println(linea);
+				contador++;
+			}
+		}else {
+			System.out.println("No es un fichero o no existe");
 		}
+
 	}
 	public void write(String nombre, File directorio, String contenido) throws IOException {
 		File miDir = new File(directorioActual(directorio)+"/"+nombre);
@@ -78,5 +87,33 @@ public class CdCommand {
 		}else {
 			throw new Exception("No existe el directorio");
 		}
+	}
+	
+	public void delete(String nombre, File directorio) throws Exception {
+		File f = new File(directorio.toString() + "/" + nombre);
+		if(f.isFile()) {
+			if (f.delete())
+			 System.out.println("El fichero " + f.toString() + " ha sido borrado correctamente");
+			else
+			 System.out.println("El fichero " + f.toString() + " no se ha podido borrar");
+		}else if(f.isDirectory()){
+			borrarDirectorio(f);
+			 
+			if (f.delete())
+			  System.out.println("El directorio '" + f.toString() + "' ha sido borrado correctamente.");
+			else
+			  System.out.println("El directorio '" + f.toString() + "' no se ha podido borrar.");
+		}
+	}
+	
+	public void borrarDirectorio(File directorio) throws Exception {
+		 File[] ficheros = directorio.listFiles();
+		 
+		 for (int x=0;x<ficheros.length;x++){
+			 if (ficheros[x].isDirectory()) {
+				  borrarDirectorio(ficheros[x]);
+				}
+			 ficheros[x].delete();
+		 }
 	}
 }
